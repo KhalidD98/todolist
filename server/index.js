75da075918b411ddd1fe7c28366142963373bb17
KhalidD98 <khalid.dakak8@gmail.com>
2021-12-02 10:04:17 -0600

    Create task request

--- server/index.js
+++ server/index.js
@@ -7,8 +7,25 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-    user: "user",
-    host: "localhost",
-    password: "root",
-    database: "employeeSystem",
-  });
+  user: "root",
+  host: "localhost",
+  password: "root",
+  database: "todolist",
+});
+
+//----- Create new task -----//
+app.post("/create", (req, res) => {
+  const task = req.body.task
+  const completed = req.body.completed
+
+  db.query("INSERT INTO todos (task, completed) VALUES(?, ?)",
+    [task, completed],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Values Inserted");
+      }
+    }
+  )
+})
