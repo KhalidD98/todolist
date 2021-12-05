31e015fa13cd1fb68781b0ea0481712cc2369951
KhalidD98 <khaliddakak@gmail.com>
2021-12-04 21:56:47 -0600

    Move tasks list to new file

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,13 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
-import Button from '@mui/material/Button';
 import axios from "axios"
-import Checkbox from '@mui/material/Checkbox';
-import DeleteIcon from '@mui/icons-material/Delete';
 import { makeStyles } from '@mui/styles';
-import Typography from '@mui/material/Typography';
 import AddTaskModal from './AddTask'
 import CheckBoxIcon from '@mui/icons-material/CheckBox';
 import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
-import EditIcon from '@mui/icons-material/Edit';
-import EditTask from './EditTask'
+import FilterMenu from './Components/FilterMenu'
+import TextField from '@mui/material/TextField';
+import TaskList from "./Components/TaskList";
 
 const useStyles = makeStyles({
   addTaskModal: {
@@ -24,27 +21,6 @@ const useStyles = makeStyles({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  tasks: {
-    backgroundColor: '#5c6bc0',
-    color: 'white',
-    width: '60vw',
-    height: '4.5rem',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    borderRadius: '100px',
-    paddingLeft: '2rem',
-    marginTop: '1.4rem',
-  },
-  taskText: {
-    paddingRight: '1rem',
-    paddingLeft: '2rem',
-  },
-  editIcon: {
-    paddingRight: '1rem',
-    paddingLeft: '1rem',
-  },
   title: {
     // marginLeft: '10rem',
     width: '60vw',
@@ -56,6 +32,9 @@ const useStyles = makeStyles({
 function App() {
   const classes = useStyles();
   const [taskList, setTaskList] = useState([])
+  const [filtered, setFiltered] = useState(false)
+  const [filteredList, setFilteredList] = useState([])
+  const [searchTerm, setSearchTerm] = useState("")
 
   useEffect(() => {
     getTasks()
@@ -65,14 +44,13 @@ function App() {
   const addTask = (task) => {
     axios.post("http://localhost:3001/create", {
       task: task,
-      completed: false
+      completed: 0
     }).then((res) => { // Update local array with new task
-      setTaskList([...taskList, { id: res.data.insertId, task: task, completed: false }])
+      setTaskList([...taskList, { id: res.data.insertId, task: task, completed: 0 }])
     })
   }
 
   //----- Get all tasks -----//
-  // TODO: Move to a useEffect for initial startup
   const getTasks = () => {
     axios.get("http://localhost:3001/todos").then((response) => {
       setTaskList(response.data)
@@ -105,6 +83,26 @@ function App() {
     axios.post(`http://localhost:3001/edit`, { task: task, id: id })
   }
 
+  //----- Filter the list -----//
+  const filterList = (filterOption) => {
+    console.log(taskList)
+    console.log(filteredList)
+    switch (filterOption) {
+      case 'completed':
+        setFiltered(true)
+        setFilteredList(taskList.filter(obj => obj.completed === 1 || obj.completed === true))
+        break
+      case 'pending':
+        setFiltered(true)
+        setFilteredList(taskList.filter(obj => obj.completed === 0 || obj.completed === false))
+        break
+      case 'all':
+        setFiltered(false)
+        setFilteredList([])
+        break
+    }
+  }
+
   return (
     <div>
 
@@ -112,22 +110,19 @@ function App() {
         KD's ToDo list
       </h2>
 
+      <TextField onChange={(e) => setSearchTerm(e.target.value)} label="Search for task..." variant="standard" />
+
       {/*----- List of Todos -----*/}
       <div className={classes.taskContainer}>
-        {taskList.slice(0).reverse().map((val, key) => { // Show tasks in reverse order (newest ones first)
-          return <div className={classes.tasks}>
-            {val.completed
-              ? <CheckBoxIcon onClick={() => updateCompleteStatus(val.id, val.completed)} />
-              : <CheckBoxOutlineBlankIcon onClick={() => updateCompleteStatus(val.id, val.completed)} />
-            }
-            <Typography className={classes.taskText}>{val.task}</Typography>
-            {/* <EditIcon onClick={() => editTask(val.id, val.task)} className={classes.editIcon} /> */}
-            <div className={classes.editIcon}>
-              <EditTask editTask={editTask} id={val.id} />
-            </div>
-            <DeleteIcon onClick={() => deleteTask(val.id)} />
-          </div>
-        })}
+        <FilterMenu filterList={filterList} />
+        <TaskList
+          editTask={editTask}
+          deleteTask={deleteTask}
+          updateCompleteStatus={updateCompleteStatus}
+          list={filtered ? filteredList : taskList}
+          searchTerm={searchTerm}
+        />
+
       </div>
 
       {/* Add Task Button */}
